31510ac63608c1a74a35c5b37e1a17ba5313dbed
daniel aguiar <bsb.aguiar@gmail.com>
2017-11-22 19:31:00 -0200

    criando profile de dev

---
+++ src/main/java/com/gestaosimples/servico/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.gestaosimples.servico.config;
+
+import java.text.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import com.gestaosimples.servico.services.DBService;
+
+@Configuration
+@Profile("Test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instanciateDataBase() throws ParseException {
+        dbService.instanciateTestDatabase();
+        return true;
+    }
+}
